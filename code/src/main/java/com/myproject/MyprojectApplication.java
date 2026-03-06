9b9a9370d89fb1f0094515f0ee91fdab9633b57c
loginkarun <loginkarun@gmail.com>
2026-03-06 12:51:10 +0530

    Auto commit: Scripts committed on 2026-03-06-07-21-09

---
+++ code/src/main/java/com/myproject/MyprojectApplication.java
@@ -0,0 +1,16 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for myproject SpringBoot application.
+ * Configured for JDK 21 and Spring Boot 3.5.9
+ */
+@SpringBootApplication
+public class MyprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyprojectApplication.class, args);
+    }
+}
